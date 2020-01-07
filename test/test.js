6d36a4d37abfc73238cfd3a42636c2c00ad1c9d4
Joyce Hao <joycehao19@gmail.com>
2020-01-07 14:58:25 -0800

    Revert "Revert "edit test.js""
    This reverts commit 3906817adb6fb8f78739583b1f14d45f427cb4a4.

--- test/test.js
+++ test/test.js
@@ -2,5 +2,10 @@ const assert = require('assert')
 
 it('should return true', () => {
   assert.equal(true, true)
-})
+});
+
+
+it('should return true', () => {
+  assert.equal(1, 1)
+});
 // .then(null, () => {console.log('something wrong')})
